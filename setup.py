3a93b5fd577ae35758e792b7283d239d60c92b81
= <=>
2025-04-18 20:47:13 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="ghas-test",
+    version="0.1",
+    install_requires=[
+        # Tutaj możesz dodać zależności, jeśli chcesz je zainstalować
+    ],
+)
